4befcc590fc0e9c075ceef391205f69e7ae63660
OmGurav66 <160343735+OmGurav66@users.noreply.github.com>
2025-08-10 18:57:44 +0530

    Add files via upload

---
+++ Calculater_task.py
@@ -0,0 +1,10 @@
+x = int(input("Enter 1st number: "))
+y = int(input("Enter 2nd number: "))
+add = x + y
+sub = x - y
+mul = x * y
+div = x // y
+print("Addition: ", add)
+print("Subtraction: ", sub)
+print("Multiplication: ", mul)
+print("Division: ", div)
---
+++ User.py
@@ -0,0 +1,4 @@
+f=str(input("Enter first Name: "))
+l=str(input("Enter last Name: "))
+fullName= f + " " + l
+print(f"Hello, {fullName} welcome to python programming!")
